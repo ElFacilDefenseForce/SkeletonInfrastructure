0b9de1793edc8dd89cea08de099011abe4f2c3c8
DerekTank <tank.derek@gmail.com>
2024-04-04 08:58:02 -0500

    Update NginxStartup.sh

--- terraform/NginxStartup.sh
+++ terraform/NginxStartup.sh
@@ -1,5 +1,65 @@
 #!/bin/bash
 apt update
-apt install -y nginx
+apt-get install -y nginx
 systemctl start nginx
 systemctl enable nginx
+apt-get install -y awscli jq
+
+mkdir -p /etc/nginx/ssl/derektank.com
+aws secretsmanager get-secret-value --secret-id derektank.com_SSL --region us-west-2 --query SecretString --output text | jq -r .certificate > /etc/nginx/ssl/derektank.com/fullchain.pem
+aws secretsmanager get-secret-value --secret-id derektank.com_SSL --region us-west-2 --query SecretString --output text | jq -r .privateKey > /etc/nginx/ssl/derektank.com/privkey.pem
+chmod 600 /etc/nginx/ssl/derektank.com/*
+
+cat <<EOT > /etc/nginx/sites-available/default
+# Port 80 Redirect
+server {
+  listen 80;
+  server_name www.derektank.com derektank.com taiga.derektank.com;
+  return 301 https://\$host\$request_uri;
+}
+# Redirect derektank.com to www.derektank.com for HTTPS
+server {
+  listen 443 ssl;
+  server_name derektank.com;
+
+  ssl_certificate /etc/nginx/ssl/derektank.com/fullchain.pem;
+  ssl_certificate_key /etc/nginx/ssl/derektank.com/privkey.pem;
+
+  return 301 https://www.derektank.com\$request_uri;
+}
+# Personal Web Site Server Block
+server {
+  listen 443 ssl;
+  server_name www.derektank.com derektank.com;
+
+  ssl_certificate /etc/nginx/ssl/derektank.com/fullchain.pem;
+  ssl_certificate_key /etc/nginx/ssl/derektank.com/privkey.pem;
+
+  location / {
+    proxy_pass http://44.213.132.37;
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+  }
+}
+#/* Taiga Web App Server Block
+#    server {
+#      listen 443 ssl;
+#      server_name taiga.derektank.com;
+#  
+#      ssl_certificate /etc/nginx/ssl/derektank.com/fullchain.pem;
+#      ssl_certificate_key /etc/nginx/ssl/derektank.com/privkey.pem;
+#  
+#      location / {
+#        proxy_pass http://<Another-EC2-Instance-IP>;
+#        proxy_set_header Host \$host;
+#        proxy_set_header X-Real-IP \$remote_addr;
+#        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+#        proxy_set_header X-Forwarded-Proto \$scheme;
+#      }
+#    }
+#*/
+EOT
+
+sudo systemctl restart nginx
