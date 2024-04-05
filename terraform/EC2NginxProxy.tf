25588f10f15b1e331a7f2999a80cc7c9a56aaef0
DerekTank <tank.derek@gmail.com>
2024-04-04 17:54:21 -0700

    Update EC2NginxProxy.tf

--- terraform/EC2NginxProxy.tf
+++ terraform/EC2NginxProxy.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "nginx_instance" {
   ami           = data.aws_ami.nginx_ami.id
   instance_type = "t3.micro"
   key_name      = "Shadrach"
-  iam_instance_profile = data.aws_iam_instance_profile.ssl_cert_access_profile.name
+  iam_instance_profile = aws_iam_instance_profile.ssl_cert_access_profile.name
   vpc_security_group_ids  = ["sg-09996be9bf1979f6c"]
 
   user_data = file("${path.module}/NginxStartup.sh")
