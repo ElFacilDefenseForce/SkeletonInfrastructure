4cdbc624d6ebc595fadfaaa12b9752b9c28720e4
DerekTank <tank.derek@gmail.com>
2024-04-05 20:02:05 -0700

    Update NginxStartup.sh

--- terraform/NginxStartup.sh
+++ terraform/NginxStartup.sh
@@ -14,52 +14,52 @@ chmod 600 /etc/nginx/ssl/derektank.com/*
 cat <<EOT > /etc/nginx/sites-available/default
 # Port 80 Redirect
 server {
-  listen 80;
-  server_name www.derektank.com derektank.com taiga.derektank.com;
-  return 301 https://\$host\$request_uri;
+    listen 80;
+    server_name derektank.com;
+    return 301 https://www.derektank.com$request_uri;
 }
-# Redirect derektank.com to www.derektank.com for HTTPS
+
+# Upgrade to HTTPS
 server {
-  listen 443 ssl;
-  server_name derektank.com;
-
-  ssl_certificate /etc/nginx/ssl/derektank.com/fullchain.pem;
-  ssl_certificate_key /etc/nginx/ssl/derektank.com/privkey.pem;
-
-  return 301 https://www.derektank.com\$request_uri;
+    listen 80;
+    server_name www.derektank.com taiga.derektank.com;
+    return 301 https://$server_name$request_uri;
 }
-# Personal Web Site Server Block
+
+#Personal Website Server Block
 server {
-  listen 443 ssl;
-  server_name www.derektank.com derektank.com;
+    listen 443 ssl;
+    server_name www.derektank.com;
 
-  ssl_certificate /etc/nginx/ssl/derektank.com/fullchain.pem;
-  ssl_certificate_key /etc/nginx/ssl/derektank.com/privkey.pem;
+    ssl_certificate /etc/nginx/ssl/derektank.com/fullchain.pem;
+    ssl_certificate_key /etc/nginx/ssl/derektank.com/privkey.pem;
 
-  location / {
-    proxy_pass http://44.213.132.37;
-    proxy_set_header Host \$host;
-    proxy_set_header X-Real-IP \$remote_addr;
-    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    proxy_set_header X-Forwarded-Proto \$scheme;
-  }
+    location / {
+        proxy_pass http://ec2-44-213-132-37.compute-1.amazonaws.com;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
 }
+
 # Taiga Web App Server Block
     server {
-      listen 443 ssl;
-      server_name taiga.derektank.com;
-  
-      ssl_certificate /etc/nginx/ssl/derektank.com/fullchain.pem;
-      ssl_certificate_key /etc/nginx/ssl/derektank.com/privkey.pem;
-  
-      location / {
-        proxy_pass http://<Another-EC2-Instance-IP>;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-      }
+    listen 443 ssl;
+    server_name taiga.derektank.com;
+
+    ssl_certificate /etc/nginx/ssl/derektank.com/fullchain.pem;
+    ssl_certificate_key /etc/nginx/ssl/derektank.com/privkey.pem;
+
+    location / {
+        proxy_pass http://8.8.8.8;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
     }
+}
+
 EOT
 
 nginx -t
