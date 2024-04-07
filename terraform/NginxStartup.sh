ab0b26bbc18eaf08270c7c2ff9063461b2b63764
DerekTank <tank.derek@gmail.com>
2024-04-06 19:16:59 -0700

    Update NginxStartup.sh

--- terraform/NginxStartup.sh
+++ terraform/NginxStartup.sh
@@ -11,56 +11,58 @@ aws secretsmanager get-secret-value --secret-id derektank.com_SSL --region us-we
 aws secretsmanager get-secret-value --secret-id derektank.com_SSL --region us-west-2 --query SecretString --output text | jq -r '.NGINXSSLPrivkey | gsub("\\\\n";"\n")' > /etc/nginx/ssl/derektank.com/privkey.pem
 chmod 600 /etc/nginx/ssl/derektank.com/*
 
-cat <<EOT > /etc/nginx/sites-available/default
-# Port 80 Redirect
-server {
-    listen 80;
-    server_name derektank.com;
-    return 301 https://www.derektank.com$request_uri;
-}
+NGINX_CONF="/etc/nginx/sites-available/default"
+echo "# Port 80 Redirect" > $NGINX_CONF
 
-# Upgrade to HTTPS
-server {
-    listen 80;
-    server_name www.derektank.com taiga.derektank.com;
-    return 301 https://$server_name$request_uri;
-}
+echo "server {" >> $NGINX_CONF
+echo "    listen 80;" >> $NGINX_CONF
+echo "    server_name derektank.com;" >> $NGINX_CONF
+echo "    return 301 https://www.derektank.com\$request_uri;" >> $NGINX_CONF
+echo "}" >> $NGINX_CONF
+echo "" >> $NGINX_CONF
 
-#Personal Website Server Block
-server {
-    listen 443 ssl;
-    server_name www.derektank.com;
+echo "# Upgrade to HTTPS" >> $NGINX_CONF
+echo "server {" >> $NGINX_CONF
+echo "    listen 80;" >> $NGINX_CONF
+echo "    server_name www.derektank.com taiga.derektank.com;" >> $NGINX_CONF
+echo "    return 301 https://\$server_name\$request_uri;" >> $NGINX_CONF
+echo "}" >> $NGINX_CONF
+echo "" >> $NGINX_CONF
 
-    ssl_certificate /etc/nginx/ssl/derektank.com/fullchain.pem;
-    ssl_certificate_key /etc/nginx/ssl/derektank.com/privkey.pem;
+echo "# Personal Website Server Block" >> $NGINX_CONF
+echo "server {" >> $NGINX_CONF
+echo "    listen 443 ssl;" >> $NGINX_CONF
+echo "    server_name www.derektank.com;" >> $NGINX_CONF
+echo "" >> $NGINX_CONF
+echo "    ssl_certificate /etc/nginx/ssl/derektank.com/fullchain.pem;" >> $NGINX_CONF
+echo "    ssl_certificate_key /etc/nginx/ssl/derektank.com/privkey.pem;" >> $NGINX_CONF
+echo "" >> $NGINX_CONF
+echo "    location / {" >> $NGINX_CONF
+echo "        proxy_pass https://ec2-44-213-132-37.compute-1.amazonaws.com;" >> $NGINX_CONF
+echo "        proxy_set_header Host \$host;" >> $NGINX_CONF
+echo "        proxy_set_header X-Real-IP \$remote_addr;" >> $NGINX_CONF
+echo "        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;" >> $NGINX_CONF
+echo "        proxy_set_header X-Forwarded-Proto \$scheme;" >> $NGINX_CONF
+echo "    }" >> $NGINX_CONF
+echo "}" >> $NGINX_CONF
+echo "" >> $NGINX_CONF
 
-    location / {
-        proxy_pass https://ec2-44-213-132-37.compute-1.amazonaws.com;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-    }
-}
-
-# Taiga Web App Server Block
-    server {
-    listen 443 ssl;
-    server_name taiga.derektank.com;
-
-    ssl_certificate /etc/nginx/ssl/derektank.com/fullchain.pem;
-    ssl_certificate_key /etc/nginx/ssl/derektank.com/privkey.pem;
-
-    location / {
-        proxy_pass https://ec2-44-213-132-37.compute-1.amazonaws.com;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-    }
-}
-
-EOT
+echo "# Taiga Web App Server Block" >> $NGINX_CONF
+echo "server {" >> $NGINX_CONF
+echo "    listen 443 ssl;" >> $NGINX_CONF
+echo "    server_name taiga.derektank.com;" >> $NGINX_CONF
+echo "" >> $NGINX_CONF
+echo "    ssl_certificate /etc/nginx/ssl/derektank.com/fullchain.pem;" >> $NGINX_CONF
+echo "    ssl_certificate_key /etc/nginx/ssl/derektank.com/privkey.pem;" >> $NGINX_CONF
+echo "" >> $NGINX_CONF
+echo "    location / {" >> $NGINX_CONF
+echo "        proxy_pass https://ec2-44-213-132-37.compute-1.amazonaws.com;" >> $NGINX_CONF
+echo "        proxy_set_header Host \$host;" >> $NGINX_CONF
+echo "        proxy_set_header X-Real-IP \$remote_addr;" >> $NGINX_CONF
+echo "        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;" >> $NGINX_CONF
+echo "        proxy_set_header X-Forwarded-Proto \$scheme;" >> $NGINX_CONF
+echo "    }" >> $NGINX_CONF
+echo "}" >> $NGINX_CONF
 
 nginx -t
 sudo systemctl restart nginx
